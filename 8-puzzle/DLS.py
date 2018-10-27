efa5eca7398fd4ea304a79553c4af5c4c94df030
Fares Mehanna <FaresMehanna9@gmail.com>
2018-10-27 18:30:49 +0200

    DLS totalnodes, depth fix

--- 8-puzzle/DLS.py
+++ 8-puzzle/DLS.py
@@ -1,33 +1,34 @@
 from Node import Node
 
 
-def search(initial_state, goal_state, limit, yield_after):
+def search(initial_state, goal_state, limit, yield_after, counter):
     cur_node = Node(initial_state)
     explored = set()
     stack = list([cur_node])
-    counter = 0
     depth = 1
     while stack:
         cur_node = stack.pop()
-        depth = max(depth, cur_node.depth)
 
+        if cur_node.depth >= limit:
+            continue;
+
+        depth = max(depth, cur_node.depth)
         explored.add(cur_node.map)
         if cur_node.is_goal(goal_state):
             break
-        if cur_node.depth < limit:
-            cur_node.expand()
-            for child in reversed(cur_node.children):
-                if child.map not in explored:
-                    stack.append(child)
-                    explored.add(child.map)
-        else:
-            continue
+
+        cur_node.expand()
+        for child in reversed(cur_node.children):
+            if child.map not in explored:
+                stack.append(child)
+                explored.add(child.map)
+
         counter += 1
         if counter % yield_after == 0:
-            yield [0, cur_node.state]
+            yield [0, cur_node.state, counter]
 
     if not cur_node.is_goal(goal_state):
-        yield [-1]
+        yield [-1, -1, counter]
 
     expanded_states = [cur_node.state]
     for parent in cur_node.ancestors():
--- 8-puzzle/IDS.py
+++ 8-puzzle/IDS.py
@@ -4,9 +4,13 @@ import DLS
 def search(initial_state, goal_state, yield_after):
     step = [-1]
     depth = 0
+    realTotalVisitedNodes = 0
+
     while step[0] != 1:
-        sol = DLS.search(initial_state, goal_state, depth, yield_after)
+        sol = DLS.search(initial_state, goal_state, depth, yield_after, realTotalVisitedNodes)
         for step in sol:
+            if step[0] != 1:
+                realTotalVisitedNodes = step[2]
             if step[0] == -1:
                 break
             yield(step)
